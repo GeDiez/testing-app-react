8f32da639f25fc13dd6731a65c90fe47c1ccc904
Gibran lm <gibran@michelada.io>
2020-06-19 21:04:31 -0500

    complete this code and have fun

--- src/__tests__/components/Login.js
+++ src/__tests__/components/Login.js
@@ -6,29 +6,14 @@ import Login from '../../components/Login';
 
 describe('Login component', function () {
   it('should render component correctly', () => {
-    render(<Login />)
+
   })
 
   it('should submit the form with email and password', async () => {
-    const onSubmit = jest.fn()
-    const {getByRole, getByLabelText, debug} = render(<Login onSubmit={onSubmit} errors={{}} />)
 
-    userEvent.type(getByLabelText('Email'), 'example@bedu.com')
-    userEvent.type(getByLabelText('Password'), 'helloWorld')
-    userEvent.click(getByRole('button'))
-
-    expect(onSubmit.mock.calls.length).toBe(1)
   })
 
   it('should show correctly fields with errors', async () => {
-    const onSubmit = jest.fn()
-    const errors = {
-      email: 'Has email error',
-      password: 'password incorrect'
-    }
-    const {getByRole, getByLabelText, getByText, debug} = render(<Login onSubmit={onSubmit} errors={errors} />)
 
-    expect(getByText('Has email error').classList.contains('is-danger')).toBeTruthy()
-    expect(getByText('password incorrect').classList.contains('is-danger')).toBeTruthy()
   })
 })
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,26 +2,21 @@ import React, {useState} from 'react'
 
 function Login (props) {
   const {onSubmit, errors} = props
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
 
+  function handleSubmit(ev) {
+    ev.preventDefault();
 
-  function handleChangeEmail(_email) {
-    setEmail(_email)
-  }
-
-  function handleChangePassword(_password) {
-    setPassword(_password)
+    // onSubmit()
   }
 
   return (
     <div className="box">
       <div className="content">
-        <form action="" onSubmit={(ev) => {ev.preventDefault(); onSubmit({email, password})}}>
+        <form action="" onSubmit={handleSubmit}>
           <div className="field">
             <label className="label" htmlFor='email'>Email</label>
             <div className="control">
-              <input id="email" className={`input ${errors?.email && 'is-danger'}`} name="email" type="email" placeholder="e.g. alexsmith@gmail.com" value={email} onChange={ev => handleChangeEmail(ev.target.value)} />
+              <input id="email" className={`input ${errors?.email && 'is-danger'}`} name="email" type="email" placeholder="e.g. alexsmith@gmail.com" />
             </div>
             <p className={`help ${errors?.email && 'is-danger'}`}>{errors?.email}</p>
           </div>
@@ -29,7 +24,7 @@ function Login (props) {
           <div className="field">
             <label className="label" htmlFor='password'>Password</label>
             <div className="control">
-              <input id='password' className={`input ${errors?.password && 'is-danger'}`} name="password" type="password" value={password} onChange={ev => handleChangePassword(ev.target.value)} />
+              <input id='password' className={`input ${errors?.password && 'is-danger'}`} name="password" type="password" />
             </div>
             <p className={`help ${errors?.password && 'is-danger'}`}>{errors?.password}</p>
           </div>
